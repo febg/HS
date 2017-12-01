5118a26842c5d6906b97e4a5b4f7caa4c57fd48e
Felipe Ballesteros <felipeballesteros1@gmail.com>
2017-12-01 15:26:14 -0800

    Update

--- HS/rooms/room.go
+++ HS/rooms/room.go
@@ -81,25 +81,16 @@ func InitializeRoom() (*Room, error) {
 	}
 
 	//Add Starting Fliped Card on the pileDeck
-	c := r.getFromDeck()
+	c, err := r.getFromDeck()
+	if err != nil {
+		log.Printf("[ERROR] getFromDeck: %v", err)
+	}
 	r.addPileCard(c)
 
 	r.ReadyToBegin = true
 
 	log.Printf("[INFO] Initialized Room: %s", id)
-	return r, id
-}
-
-func (r *Room) addPileCard(c deck.Card) {
-	c.FaceUp = true
-	r.PileDeck.Cards = append(r.PileDeck.Cards, c)
-
-}
-
-func (r *Room) getFromDeck() deck.Card {
-	c := r.CurrentDeck.Cards[len(r.CurrentDeck.Cards)-1]
-	r.CurrentDeck.Cards = r.CurrentDeck.Cards[:len(r.CurrentDeck.Cards)-1]
-	return c
+	return r, nil
 }
 
 //AddPlayer adds a given player to a room
@@ -166,87 +157,6 @@ func (r *Room) RemovePlayer(p *Player) error {
 	return nil
 }
 
-//Deal gives the current players in the room a new hand with 3 visible cards and 3 non-visible cards
-func (r *Room) Deal() {
-	//empty each player's hand
-	for _, player := range r.Players {
-		player.CurrentHand = []deck.Card{}
-	}
-
-	for i := 0; i < CardsPerHand; i++ {
-		//deal one card to each player
-		for _, player := range r.Players {
-			//make the card at the top of the deck visible to its future owner if its one of the first 3 cards the player has
-			if i < VisibleCardsPerHand {
-				r.CurrentDeck.Cards[len(r.CurrentDeck.Cards)-1].VisibleToOwner = true
-			}
-			//give the current player the card at the top of the deck
-			player.CurrentHand = append(player.CurrentHand, r.CurrentDeck.Cards[len(r.CurrentDeck.Cards)-1])
-			//pop the card given off the deck
-			r.CurrentDeck.Cards = r.CurrentDeck.Cards[:len(r.CurrentDeck.Cards)-1]
-		}
-	}
-}
-
-func (r *Room) NewCard(p *Player) {
-	p.NewCard = r.CurrentDeck.Cards[len(r.CurrentDeck.Cards)-1]
-	p.NewCard.VisibleToOwner = true
-	r.CurrentDeck.Cards = r.CurrentDeck.Cards[:len(r.CurrentDeck.Cards)-1]
-}
-
-func (r *Room) NewPileCard(p *Player) {
-	log.Printf("pileDeck: %+v", r.PileDeck)
-	p.NewCard = r.PileDeck.Cards[len(r.PileDeck.Cards)-1]
-	log.Println("After new card")
-	p.NewCard.VisibleToOwner = true
-	r.PileDeck.Cards = r.PileDeck.Cards[:len(r.PileDeck.Cards)-1]
-}
-
-func (r *Room) SelectCard(p *Player) {
-	for i, c := range p.CurrentHand {
-		if c.Type == r.Response.FlipCardType && c.Suit == r.Response.FlipCardSuit {
-			card := &p.CurrentHand[i]
-			card.FlipUp()
-			log.Println("after trying to flip")
-			log.Println(card)
-		}
-	}
-}
-
-func (r *Room) FlipRandomCard(p *Player) {
-
-	//get a random index number for a player
-	var d deck.Deck
-	for _, v := range p.CurrentHand {
-		if !v.FaceUp {
-			d.Cards = append(d.Cards, v)
-		}
-	}
-
-	//Test Rand Only
-	var randi int
-
-	var c *deck.Card
-	if !(len(d.Cards) == 1) {
-		rand.Seed(time.Now().UnixNano())
-		i := rand.Intn(len(d.Cards) - 1)
-		randi = i
-		c = &d.Cards[i]
-	} else {
-		c = &d.Cards[0]
-		randi = 0
-	}
-	p.RandSeq = append(p.RandSeq, randi)
-	for idx, v := range p.CurrentHand {
-		if v.Type == c.Type && v.Suit == c.Suit {
-			card := &p.CurrentHand[idx]
-			card.FlipUp()
-			//log.Printf("after trying to flip: %v\n", c)
-			//log.Println(c)
-		}
-	}
-}
-
 //CanBeginNextRound returns true if there is enough players for a game and the room is ready
 func (r *Room) CanBeginNextRound() bool {
 	return r.ReadyToBegin && len(r.Players) >= MinPlayersForGame
@@ -265,7 +175,7 @@ func (r *Room) StartRound() error {
 	r.Lock()
 	log.Printf("[INFO] Started Round on Room: %s\n", r.RoomID)
 	//deal the cards to the players in the room
-	r.Deal()
+	r.deal()
 
 	for _, v := range r.Players {
 		log.Printf("Player %v: %+v \n\n", v.PlayerID, v.CurrentHand)
@@ -321,43 +231,12 @@ func (r *Room) StartRound() error {
 	return nil
 }
 
-func (r *Room) getRandomPlayerName() string {
-	rand.Seed(time.Now().UnixNano())
-	//get a random index number for a player
-	i := rand.Intn(len(r.Players) - 1)
-	return r.Players[i].PlayerID
-}
-
-//getNextPlayerName returns the name of the player next in order
-func (r *Room) getNextPlayerID() (string, error) {
-	for idx, player := range r.Players { //lock for room is held so OK
-		//read the id of the player
-		playerName := player.PlayerID
-		if playerName == r.PlayerInControl {
-			//if the index is that of the last
-			if idx == len(r.Players)-1 {
-				//read the id of the 0th player in the array and return it
-				name := r.Players[0].PlayerID
-				return name, nil
-			}
-			//read the id of the next player in the array and return it
-			name := r.Players[idx+1].PlayerID
-			return name, nil
-		}
-	}
-	return "", errors.New("Player in control name was not found")
-}
-
-func (r *Room) GetPlayer(pID string) {
-	//TODO
-}
-
 func (r *Room) HandleTimeRanOut() {
 	for _, p := range r.Players {
 		if p.PlayerID == r.PlayerInControl {
-			r.NewCard(p)
+			r.newCard(p)
 			r.addPileCard(p.NewCard) //Get new card, put it on pile, flip random
-			r.FlipRandomCard(p)
+			r.flipRandomCard(p)
 			p.NewCard = deck.Card{} //nil card
 			r.PlayerInControlReady = true
 		}
@@ -369,21 +248,21 @@ func (r *Room) HandleGotResponse() {
 	case NEW:
 		for _, p := range r.Players {
 			if p.PlayerID == r.Response.PlayerID {
-				r.NewCard(p)
+				r.newCard(p)
 			}
 		}
 	case OLD:
 		for _, p := range r.Players {
 			if p.PlayerID == r.Response.PlayerID {
 				log.Println("Entered old case going into newpilecard")
-				r.NewPileCard(p)
+				r.newPileCard(p)
 			}
 		}
 	case FLIP:
 		for _, p := range r.Players {
 			if p.PlayerID == r.Response.PlayerID {
 				log.Println("Entered old case going into selectCard")
-				r.SelectCard(p)
+				r.selectCard(p)
 				log.Println("Again in old case going into addPileCard")
 				r.addPileCard(p.NewCard)
 				p.NewCard = deck.Card{} //nil card
@@ -398,22 +277,6 @@ func (r *Room) HandleGotResponse() {
 			}
 		}
 	}
-}
-
-func (r *Room) replaceCard(p *Player) {
-	for i, c := range p.CurrentHand {
-		if c.Type == r.Response.FlipCardType && c.Suit == r.Response.FlipCardSuit {
-			log.Println("Entered if inside replace card")
-			card := &p.NewCard
-			card.FaceUp = true
-			p.CurrentHand[i] = p.NewCard
-			r.addPileCard(c)
-			r.PlayerInControlReady = true
-			p.NewCard = deck.Card{}
-			return
-		}
-	}
-	log.Print("-> [ERROR] Unable to find card on player hand")
 }
 
 func (r *Room) StartObserver() {
@@ -438,4 +301,146 @@ func (r *Room) StartObserver() {
 		//sleep
 		time.Sleep(time.Second * 2) //2 for now and will make shorter if works
 	}
+}
+
+func (r *Room) GetPlayer(pID string) {
+	//TODO
+}
+
+func (r *Room) addPileCard(c deck.Card) {
+	c.FaceUp = true
+	r.PileDeck.Cards = append(r.PileDeck.Cards, c)
+}
+
+func (r *Room) flipRandomCard(p *Player) {
+
+	//get a random index number for a player
+	var d deck.Deck
+	for _, v := range p.CurrentHand {
+		if !v.FaceUp {
+			d.Cards = append(d.Cards, v)
+		}
+	}
+
+	//Test Rand Only
+	var randi int
+
+	var c *deck.Card
+	if !(len(d.Cards) == 1) {
+		rand.Seed(time.Now().UnixNano())
+		i := rand.Intn(len(d.Cards) - 1)
+		randi = i
+		c = &d.Cards[i]
+	} else {
+		c = &d.Cards[0]
+		randi = 0
+	}
+	p.RandSeq = append(p.RandSeq, randi)
+	for idx, v := range p.CurrentHand {
+		if v.Type == c.Type && v.Suit == c.Suit {
+			card := &p.CurrentHand[idx]
+			card.FlipUp()
+			//log.Printf("after trying to flip: %v\n", c)
+			//log.Println(c)
+		}
+	}
+}
+
+func (r *Room) getFromDeck() (deck.Card, error) {
+	if len(r.CurrentDeck.Cards) == 0 {
+		return deck.Card{}, errors.New("Error")
+	}
+	c := r.CurrentDeck.Cards[len(r.CurrentDeck.Cards)-1]
+	r.CurrentDeck.Cards = r.CurrentDeck.Cards[:len(r.CurrentDeck.Cards)-1]
+	return c, nil
+}
+
+//getNextPlayerName returns the name of the player next in order
+func (r *Room) getNextPlayerID() (string, error) {
+	for idx, player := range r.Players { //lock for room is held so OK
+		//read the id of the player
+		playerName := player.PlayerID
+		if playerName == r.PlayerInControl {
+			//if the index is that of the last
+			if idx == len(r.Players)-1 {
+				//read the id of the 0th player in the array and return it
+				name := r.Players[0].PlayerID
+				return name, nil
+			}
+			//read the id of the next player in the array and return it
+			name := r.Players[idx+1].PlayerID
+			return name, nil
+		}
+	}
+	return "", errors.New("Player in control name was not found")
+}
+
+//Deal gives the current players in the room a new hand with 3 visible cards and 3 non-visible cards
+func (r *Room) deal() {
+	//empty each player's hand
+	for _, player := range r.Players {
+		player.CurrentHand = []deck.Card{}
+	}
+
+	for i := 0; i < CardsPerHand; i++ {
+		//deal one card to each player
+		for _, player := range r.Players {
+			//make the card at the top of the deck visible to its future owner if its one of the first 3 cards the player has
+			if i < VisibleCardsPerHand {
+				r.CurrentDeck.Cards[len(r.CurrentDeck.Cards)-1].VisibleToOwner = true
+			}
+			//give the current player the card at the top of the deck
+			player.CurrentHand = append(player.CurrentHand, r.CurrentDeck.Cards[len(r.CurrentDeck.Cards)-1])
+			//pop the card given off the deck
+			r.CurrentDeck.Cards = r.CurrentDeck.Cards[:len(r.CurrentDeck.Cards)-1]
+		}
+	}
+}
+
+func (r *Room) newCard(p *Player) {
+	p.NewCard = r.CurrentDeck.Cards[len(r.CurrentDeck.Cards)-1]
+	p.NewCard.VisibleToOwner = true
+	r.CurrentDeck.Cards = r.CurrentDeck.Cards[:len(r.CurrentDeck.Cards)-1]
+}
+
+func (r *Room) newPileCard(p *Player) {
+	log.Printf("pileDeck: %+v", r.PileDeck)
+	p.NewCard = r.PileDeck.Cards[len(r.PileDeck.Cards)-1]
+	log.Println("After new card")
+	p.NewCard.VisibleToOwner = true
+	r.PileDeck.Cards = r.PileDeck.Cards[:len(r.PileDeck.Cards)-1]
+}
+
+func (r *Room) selectCard(p *Player) {
+	for i, c := range p.CurrentHand {
+		if c.Type == r.Response.FlipCardType && c.Suit == r.Response.FlipCardSuit {
+			card := &p.CurrentHand[i]
+			card.FlipUp()
+			log.Println("after trying to flip")
+			log.Println(card)
+		}
+	}
+}
+
+func (r *Room) replaceCard(p *Player) {
+	for i, c := range p.CurrentHand {
+		if c.Type == r.Response.FlipCardType && c.Suit == r.Response.FlipCardSuit {
+			log.Println("Entered if inside replace card")
+			card := &p.NewCard
+			card.FaceUp = true
+			p.CurrentHand[i] = p.NewCard
+			r.addPileCard(c)
+			r.PlayerInControlReady = true
+			p.NewCard = deck.Card{}
+			return
+		}
+	}
+	log.Print("-> [ERROR] Unable to find card on player hand")
+}
+
+func (r *Room) getRandomPlayerName() string {
+	rand.Seed(time.Now().UnixNano())
+	//get a random index number for a player
+	i := rand.Intn(len(r.Players) - 1)
+	return r.Players[i].PlayerID
 }
